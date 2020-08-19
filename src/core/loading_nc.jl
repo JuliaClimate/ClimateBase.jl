4938ce01aad3a564c2bbe37e6c314ed5685f104d
Datseris <datseris.george@gmail.com>
2020-08-19 14:48:10 +0200

    Motivate usage of `NCDataset` to load files

--- src/core/loading_nc.jl
+++ src/core/loading_nc.jl
@@ -4,6 +4,7 @@ An initial version of parts of this code was taken from:
 https://github.com/rafaqz/GeoData.jl
 =#
 using NCDatasets
+export NCDataset
 export nckeys, ncdetails
 #########################################################################
 # NCDatasets → DimensionalArray convertions and loading
@@ -44,51 +45,67 @@ function ncdetails(file::String, io = stdout)
 end
 
 """
-    ClimArray(file::String, var::String; eqarea = false)) -> A, attrib
-Load the variable `var` from the `.nc` file located at `file` and convert it
-into a `ClimArray` which contains the variable attributes as a dictionary.
+    ClimArray(file::NCDataset, var::String; eqarea = false)) -> A
+Load the variable `var` from the `file` and convert it
+into a `ClimArray` which also contains the variable attributes as a dictionary.
 
-Notice that if there are no missing values in the data (according to CF standards), the
+Notice that `file` should be an `NCDataset`, which allows you to lazily combine different
+`.nc` data (typically split by time), e.g.
+```julia
+alldata = ["toa_fluxes_2020_$(i).nc" for i in 1:12]
+file = NCDataset(alldata; aggdim = "time")
+A = ClimArray(file, "tow_sw_all")
+```
+(of course you can just do `NCDataset("file.nc")` for single files).
+
+If there are no missing values in the data (according to CF standards), the
 returned array is automatically converted to a concrete type (i.e. `Union{Float32, Missing}`
 becomes `Float32`).
 
-Keyword `eqarea` denotes if the underlying grid is structured or unstructured (equal area).
+Keyword `eqarea` denotes if the underlying grid is lon-lat or equal area.
 """
-function ClimArray(path::String, var::String; eqarea = false)
+function ClimArray(path::Union{String, Vector{String}}, args...; kwargs...)
     NCDataset(path) do ds
-        svar = string(var)
-        cfvar = ds[svar]
-        attrib = Dict(cfvar.attrib)
-        A = cfvar |> Array
-        # TODO: I have to re-work this code to be more general and allow other dimensions
-        # as well!!!!
-        if eqarea
-            if haskey(ds, "ncells") # this is the equal area grid, so we make a Coord dimension
-                lon = ds["lon"] |> Array .|> wrap_lon
-                lat = ds["lat"] |> Array
-                time = ds["time"] |> Array
-                lonlat = [SVector(lon[i], lat[i]) for i in 1:length(lon)]
-                # here we sort lonlat and A in ascending latitude order,
-                # because the CDO output has reverse or even totally unsorted order
-                si = sortperm(lonlat, by = x -> x[2])
-                data = ClimArray(A[si, :], (Coord(lonlat[si]), Time(time));
-                attrib = attrib, name = svar)
-            elseif haskey(ds, "reduced_points")
-                lonlat = reduced_grid_to_points(ds["lat"], ds["reduced_points"])
-                si = sortperm(lonlat, by = x -> x[2])
-                time = ds["time"] |> Array
-                data = ClimArray(A[si, :], (Coord(lonlat[si]), Time(time));
-                name = svar, attrib = attrib)
-            end
-        else # standard variables
-            dnames = Tuple(NCDatasets.dimnames(cfvar))
-            data = ClimArray(A, create_dims(ds, dnames); name = svar, attrib = attrib)
-        end
-        if !any(ismissing, data)
-            data = nomissing(data)
-        end
+        data = ClimArray(ds, args...; kwargs...)
         return data
     end
+end
+
+function ClimArray(ds::NCDataset, var::String; eqarea = false)
+    svar = string(var)
+    cfvar = ds[svar]
+    attrib = Dict(cfvar.attrib)
+    A = cfvar |> Array
+    if eqarea
+        # TODO: I have to re-work this code to be more general and allow other dimensions
+        # as well!!!!
+        if haskey(ds, "ncells") # this is the equal area grid, so we make a Coord dimension
+            lon = ds["lon"] |> Array .|> wrap_lon
+            lat = ds["lat"] |> Array
+            time = ds["time"] |> Array
+            lonlat = [SVector(lon[i], lat[i]) for i in 1:length(lon)]
+            # here we sort lonlat and A in ascending latitude order,
+            # because the CDO output has reverse or even totally unsorted order
+            si = sortperm(lonlat, by = x -> x[2])
+            data = ClimArray(A[si, :], (Coord(lonlat[si]), Time(time));
+            attrib = attrib, name = svar)
+        elseif haskey(ds, "reduced_points")
+            lonlat = reduced_grid_to_points(ds["lat"], ds["reduced_points"])
+            si = sortperm(lonlat, by = x -> x[2])
+            time = ds["time"] |> Array
+            data = ClimArray(A[si, :], (Coord(lonlat[si]), Time(time));
+            name = svar, attrib = attrib)
+        else
+            error("Don't know how to handle this equal area grid!")
+        end
+    else # standard variables
+        dnames = Tuple(NCDatasets.dimnames(cfvar))
+        data = ClimArray(A, create_dims(ds, dnames); name = svar, attrib = attrib)
+    end
+    if !any(ismissing, data)
+        data = nomissing(data)
+    end
+    return data
 end
 
 #########################################################################
