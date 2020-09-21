7a3f6eae47ce06e4586631a8929c3ad217fd528c
Datseris <datseris.george@gmail.com>
2020-09-21 11:33:57 +0200

    Add theme of JuliaClimate to the docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,20 +7,20 @@ using ClimateBase
 using Documenter, DimensionalData, NCDatasets, StatsBase
 
 # %%
-# using DocumenterTools: Themes
+using DocumenterTools: Themes
 # download the themes
-# for file in ("juliadynamics-lightdefs.scss", "juliadynamics-darkdefs.scss", "juliadynamics-style.scss")
-#     download("https://raw.githubusercontent.com/JuliaDynamics/doctheme/master/$file", joinpath(@__DIR__, file))
-# end
-# # create the themes
-# for w in ("light", "dark")
-#     header = read(joinpath(@__DIR__, "juliadynamics-style.scss"), String)
-#     theme = read(joinpath(@__DIR__, "juliadynamics-$(w)defs.scss"), String)
-#     write(joinpath(@__DIR__, "juliadynamics-$(w).scss"), header*"\n"*theme)
-# end
-# # compile the themes
-# Themes.compile(joinpath(@__DIR__, "juliadynamics-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
-# Themes.compile(joinpath(@__DIR__, "juliadynamics-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
+for file in ("juliaclimate-lightdefs.scss", "juliaclimate-darkdefs.scss", "juliaclimate-style.scss")
+    download("https://raw.githubusercontent.com/JuliaClimate/doctheme/master/$file", joinpath(@__DIR__, file))
+end
+# create the themes
+for w in ("light", "dark")
+    header = read(joinpath(@__DIR__, "juliaclimate-style.scss"), String)
+    theme = read(joinpath(@__DIR__, "juliaclimate-$(w)defs.scss"), String)
+    write(joinpath(@__DIR__, "juliaclimate-$(w).scss"), header*"\n"*theme)
+end
+# compile the themes
+Themes.compile(joinpath(@__DIR__, "juliaclimate-light.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-light.css"))
+Themes.compile(joinpath(@__DIR__, "juliaclimate-dark.scss"), joinpath(@__DIR__, "src/assets/themes/documenter-dark.css"))
 
 
 # %% Build the actual docs
@@ -30,10 +30,10 @@ makedocs(modules = [ClimateBase, DimensionalData],
     doctest = false,
     format = Documenter.HTML(
         prettyurls = CI,
-        # assets = [
-        #     "assets/logo.ico",
-        #     asset("https://fonts.googleapis.com/css?family=Quicksand|Montserrat|Source+Code+Pro|Lora&display=swap", class=:css),
-        #     ],
+        assets = [
+            # "assets/logo.ico",
+            asset("https://fonts.googleapis.com/css?family=Quicksand|Montserrat|Source+Code+Pro|Lora&display=swap", class=:css),
+            ],
         ),
     pages = [
         "Introduction" => "index.md",
