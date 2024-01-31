034c26ad843ba08a2a274fa75dddedbd2eacdccb
Datseris <datseris.george@gmail.com>
2024-01-31 15:49:17 +0000

    Update dependencies, move to Julia extensions

--- src/ClimateBase.jl
+++ src/ClimateBase.jl
@@ -21,12 +21,6 @@ include("tsa/continuation.jl")
 include("tsa/decomposition.jl")
 
 include("exports.jl")
-
-using Requires
-function __init__()
-    @require GeoMakie="db073c08-6b98-4ee5-b6a4-5efafb3259c6" begin
-        include("plotting/geomakie.jl")
-    end
-end
+include("plotting/geomakie.jl")
 
 end # module
